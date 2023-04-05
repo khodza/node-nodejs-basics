59c56960d8804647e9ae95deb1f17c21d02f54cc
Izzattillakhodza Makhmudkhodzaev <khodzapro@gmail.com>
2023-04-05 10:02:33 +0500

    it is done with fs

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    //In order to see that it works you should add global env variables 
+    const envVariables = Object.entries(process.env);
+  envVariables.forEach(([key, value]) => {
+    if (key.startsWith('RSS_')) {
+      console.log(`RSS_${key.slice(4)}=${value}`);
+    }
+  });
 };
 
 parseEnv();
